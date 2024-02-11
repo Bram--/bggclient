c11bf419c4c6b497edf8828e497e7626c3b75485
Bram Wijnands <brambomail@gmail.com>
2024-02-11 20:49:54 +0000

    Adds a Jetpack Compose Android example for BGGClient.

---
+++ examples/android/app/src/main/java/org/audux/bgg/examples/android/ui/theme/Theme.kt
@@ -0,0 +1,46 @@
+package org.audux.bgg.examples.android.ui.theme
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+private val DarkColorScheme =
+    darkColorScheme(
+        primary = Purple80,
+        secondary = PurpleGrey80,
+        tertiary = Pink80,
+    )
+
+private val LightColorScheme =
+    lightColorScheme(
+        primary = Purple40,
+        secondary = PurpleGrey40,
+        tertiary = Pink40,
+    )
+
+@Composable
+fun AndroidTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+  val colorScheme =
+      when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+      }
+  val view = LocalView.current
+  if (!view.isInEditMode) {
+    SideEffect {
+      val window = (view.context as Activity).window
+      window.statusBarColor = Color.Transparent.toArgb()
+      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+    }
+  }
+
+  MaterialTheme(colorScheme = colorScheme, typography = Typography, content = content)
+}

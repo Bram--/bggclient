e7f0c99a8619589ebb8991bdccaefd163c639c40
Bram Wijnands <brambomail@gmail.com>
2024-02-16 13:16:45 +0000

    Minor cleanup and changes to config.

--- examples/java/build.gradle.kts
+++ examples/java/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
+
 tasks.named<JavaExec>("run") {
     standardInput = System.`in`
 }

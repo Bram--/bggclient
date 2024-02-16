0af5bacf978c5116fd69a3b7c2f0eac840f68712
Bram Wijnands <brambomail@gmail.com>
2024-02-16 13:32:33 +0000

    Bump version.

--- examples/paginate/build.gradle.kts
+++ examples/paginate/build.gradle.kts
@@ -20,7 +20,7 @@ application {
 }
 
 dependencies {
-    implementation("org.audux.bgg:bggclient:0.4")
+    implementation("org.audux.bgg:bggclient:0.4.1")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }

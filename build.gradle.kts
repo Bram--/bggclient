1b1643e8fda8ee13547c18454455012b081f4301
Bram Wijnands <brambomail@gmail.com>
2024-02-22 16:30:01 +0000

    Bump version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "org.audux.bgg"
             artifactId = "bggclient"
-            version = "0.4.3"
+            version = "0.4.4"
 
             pom {
                 name = "Unofficial JVM BGG client"
--- examples/android/app/build.gradle.kts
+++ examples/android/app/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
-    implementation("org.audux.bgg:bggclient:0.4.3")
+    implementation("org.audux.bgg:bggclient:0.4.4")
 
     testImplementation("junit:junit:4.13.2")
 

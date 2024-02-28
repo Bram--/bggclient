d2074f987b8c8e5b57ec96fec8970135ac515dc2
Bram-- <brambomail@gmail.com>
2024-02-28 22:26:58 +0000

    Update build.gradle.kts with publishing repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,17 @@ publishing {
             from(components["java"])
         }
     }
+
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
 }
 
 ktfmt {

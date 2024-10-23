de2ff5519adbd777a46f60a5e1ce8ca3089c172f
Bram Wijnands <brambomail@gmail.com>
2024-10-23 13:34:53 +0100

    Ensure requests still wait if others are already being handled. Also retain request order.

--- src/main/kotlin/org/audux/bgg/plugin/ClientRateLimitPlugin.kt
+++ src/main/kotlin/org/audux/bgg/plugin/ClientRateLimitPlugin.kt
@@ -16,14 +16,16 @@ package org.audux.bgg.plugin
 import co.touchlab.kermit.Logger
 import io.ktor.client.plugins.api.createClientPlugin
 import io.ktor.client.request.HttpRequestBuilder
+import kotlinx.coroutines.delay
+import org.jetbrains.annotations.VisibleForTesting
 import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.atomic.AtomicReference
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.nanoseconds
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.TimeMark
 import kotlin.time.TimeSource
-import kotlinx.coroutines.delay
-import org.jetbrains.annotations.VisibleForTesting
 
 /**
  * Ktor client rate limit plugin.
@@ -32,50 +34,57 @@ import org.jetbrains.annotations.VisibleForTesting
  * example the plugin can be configured to do 60 requests per minute, or 1 request per second etc.
  */
 internal val ClientRateLimitPlugin =
-    createClientPlugin(
-        "ClientRateLimitPlugin",
-        createConfiguration = ::RequestLimiterConfiguration,
-    ) {
-        val requestLimiter = RequestLimiter(pluginConfig.requestLimit, pluginConfig.windowSize)
-        onRequest { request, _ -> requestLimiter.onNewRequest(request) }
-    }
+  createClientPlugin(
+    "ClientRateLimitPlugin",
+    createConfiguration = ::RequestLimiterConfiguration,
+  ) {
+    val requestLimiter = RequestLimiter(pluginConfig.requestLimit, pluginConfig.windowSize)
+    onRequest { request, _ -> requestLimiter.onNewRequest(request) }
+  }
 
 /**
  * Implementation of [ClientRateLimitPlugin] ensuring not more than [requestLimit] are being within
  * the period of [windowLength].
  */
 internal class RequestLimiter(private val requestLimit: Int, private val windowLength: Duration) {
-    private val timeSource: TimeSource = TimeSource.Monotonic
-    private var currentWindowStart: AtomicReference<TimeMark?> = AtomicReference()
+  private val timeSource: TimeSource = TimeSource.Monotonic
+  private var currentWindowStart: AtomicReference<TimeMark?> = AtomicReference()
+  private var requestsInDelayed: AtomicInteger = AtomicInteger(0)
 
-    @VisibleForTesting internal var requestsInCurrentWindow: AtomicInteger = AtomicInteger(0)
+  @VisibleForTesting
+  internal var requestsInCurrentWindow: AtomicInteger = AtomicInteger(0)
 
-    suspend fun onNewRequest(request: HttpRequestBuilder) {
-        var windowStart = currentWindowStart.get()
-        if (windowStart == null || windowStart.plus(windowLength).hasPassedNow()) {
-            windowStart = timeSource.markNow()
-            currentWindowStart.set(windowStart)
-        }
-        logger.v(tag = "RequestLimiter") { "#OnNewRequest()" }
+  suspend fun onNewRequest(request: HttpRequestBuilder) {
+    do {
+      var windowStart = currentWindowStart.get()
+      if (windowStart == null || windowStart.plus(windowLength).hasPassedNow()) {
+        windowStart = timeSource.markNow()
+        currentWindowStart.set(windowStart)
+        requestsInCurrentWindow.set(0)
+      }
 
-        val requestsMadeInWindow = requestsInCurrentWindow.get()
-        if (requestsMadeInWindow < requestLimit) {
-            requestsInCurrentWindow.incrementAndGet()
-        } else {
-            logger.i(tag = "RequestLimiter") {
-                "Requests limit for window reached[$requestsMadeInWindow/$requestLimit]"
-            }
+      val requestsMadeInWindow = requestsInCurrentWindow.get()
+      if (requestsMadeInWindow < requestLimit) {
+        requestsInCurrentWindow.incrementAndGet()
+        break
+      }
 
-            delay(windowLength.minus(windowStart.elapsedNow()))
-            logger.v(tag = "RequestLimiter") {
-                "Delay completed ${windowStart.elapsedNow().inWholeMilliseconds}"
-            }
-        }
-    }
+      val requestsBeingDelayed = requestsInDelayed.incrementAndGet()
+      logger.i(tag = "RequestLimiter") {
+        "Requests limit for window reached[$requestsMadeInWindow/$requestLimit]"
+      }
 
-    companion object {
-        private val logger = Logger.withTag("ClientRateLimitPlugin")
-    }
+      // Add `requestsBeingDelayed`ms to the delay to ensure requests retain order after delay.
+      delay(
+        windowLength.minus(windowStart.elapsedNow()).plus(requestsBeingDelayed.milliseconds)
+      )
+      requestsBeingDelayed.dec()
+    } while (true)
+  }
+
+  companion object {
+    private val logger = Logger.withTag("ClientRateLimitPlugin")
+  }
 }
 
 /**
@@ -87,6 +96,6 @@ internal class RequestLimiter(private val requestLimit: Int, private val windowL
  *   "60 requests per 60.seconds".
  */
 internal data class RequestLimiterConfiguration(
-    var requestLimit: Int = 60,
-    var windowSize: Duration = 60.seconds,
+  var requestLimit: Int = 60,
+  var windowSize: Duration = 60.seconds,
 )
--- src/test/kotlin/org/audux/bgg/plugin/ClientRateLimitPluginTest.kt
+++ src/test/kotlin/org/audux/bgg/plugin/ClientRateLimitPluginTest.kt
@@ -19,153 +19,161 @@ import io.ktor.client.engine.mock.MockEngine
 import io.ktor.client.engine.mock.MockEngineConfig
 import io.ktor.client.plugins.api.createClientPlugin
 import io.ktor.client.request.get
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.milliseconds
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.audux.bgg.util.TestUtils.instantResponse
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 
 /** Tests for [ClientConcurrentRateLimitPlugin] and [ConcurrentRequestLimiter]. */
 class ClientRateLimitPluginTest {
-    private lateinit var requestLimiter: RequestLimiter
+  private lateinit var requestLimiter: RequestLimiter
 
-    @BeforeEach
-    fun beforeEach() {
-        AtomicSingletonInteger.instance.reset()
+  @BeforeEach
+  fun beforeEach() {
+    AtomicSingletonInteger.instance.reset()
+  }
+
+  @Test
+  fun `Enqueue incoming requests that do not exceed the request limit for the current window`() {
+    val client = createClient(requestLimit = 10) { repeat(4) { addHandler(instantResponse()) } }
+
+    val jobs = runBlocking {
+      // Launch 4 requests.
+      val jobs = (0..3).map { launch { client.get("/") } }
+
+      delay(2)
+
+      // Ensure 2 requests are in-flight and 2 are queued.
+      assertThat(requestLimiter.requestsInCurrentWindow.get()).isEqualTo(4)
+
+      jobs
     }
 
-    @Test
-    fun `Enqueue incoming requests that do not exceed the request limit for the current window`() {
-        val client = createClient(requestLimit = 10) { repeat(4) { addHandler(instantResponse()) } }
+    // After all coroutines have finished ensure 3 requests are made
+    val engine = client.engine as MockEngine
+    assertThat(engine.requestHistory).hasSize(4)
+    assertAllJobsAre(jobs) { !isActive }
+    assertAllJobsAre(jobs) { !isCancelled }
+    assertAllJobsAre(jobs) { isCompleted }
+  }
 
-        val jobs = runBlocking {
-            // Launch 4 requests.
-            val jobs = (0..3).map { launch { client.get("/") } }
+  @Test
+  fun `Does not enqueue incoming requests that exceed the request limit for the current window`() {
+    val client = createClient(requestLimit = 2) { repeat(4) { addHandler(instantResponse()) } }
 
-            delay(2)
+    val jobs = runBlocking {
+      // Launch 4 requests.
+      val jobs = (0..3).map { launch { client.get("/") } }
 
-            // Ensure 2 requests are in-flight and 2 are queued.
-            assertThat(requestLimiter.requestsInCurrentWindow.get()).isEqualTo(4)
+      delay(2)
 
-            jobs
+      // Ensure 2 requests are in-flight and 2 are queued.
+      assertThat(requestLimiter.requestsInCurrentWindow.get()).isEqualTo(2)
+
+      jobs
+    }
+
+    // After all coroutines have finished ensure 3 requests are made
+    val engine = client.engine as MockEngine
+    assertThat(engine.requestHistory).hasSize(4)
+    assertAllJobsAre(jobs) { !isActive }
+    assertAllJobsAre(jobs) { !isCancelled }
+    assertAllJobsAre(jobs) { isCompleted }
+  }
+
+  @RepeatedTest(100)
+  fun `delays requests to the new window`() {
+    val client = createClient(requestLimit = 2) { repeat(5) { addHandler(instantResponse()) } }
+
+    runBlocking {
+      val job1 = launch { client.get("/1") }
+      val job2 = launch { client.get("/2") }
+      delay(2) // ensure jobs are running
+
+      val job3 = launch { client.get("/3") }
+      val job4 = launch { client.get("/4") }
+      val job5 = launch { client.get("/5") }
+      assertThat(requestLimiter.requestsInCurrentWindow.get()).isEqualTo(2)
+
+      delay(10) // ensure jobs 1 and 2 are completed
+
+      assertThat(job1.isCompleted).isTrue()
+      assertThat(job2.isCompleted).isTrue()
+      assertThat(job3.isActive).isTrue()
+      assertThat(job4.isActive).isTrue()
+      assertThat(job5.isActive).isTrue()
+
+      delay(20) // ensure jobs 3 and 4 are completed
+
+      assertThat(job1.isCompleted).isTrue()
+      assertThat(job2.isCompleted).isTrue()
+      assertThat(job3.isCompleted).isTrue()
+      assertThat(job4.isCompleted).isTrue()
+      assertThat(job5.isActive).isTrue()
+
+      delay(30) // ensure job 5 is completed.
+
+      assertThat(job5.isCompleted).isTrue()
+    }
+
+    val engine = client.engine as MockEngine
+    assertThat(engine.requestHistory).hasSize(5)
+  }
+
+  @Test
+  fun `Enqueues incoming requests that would exceed the requests limit even for different clients`() {
+    val clients =
+      listOf(
+        createClient(requestLimit = 2) { addHandler(instantResponse()) },
+        createClient(requestLimit = 2) { addHandler(instantResponse()) },
+        createClient(requestLimit = 2) { addHandler(instantResponse()) },
+        createClient(requestLimit = 2) { addHandler(instantResponse()) },
+        createClient(requestLimit = 2) { addHandler(instantResponse()) },
+      )
+
+    val jobs = runBlocking {
+      // Launch 5 concurrent requests
+      val jobs = clients.map { client -> launch { client.get("/") } }
+
+      // Minor delay to ensure client.get calls are done
+      delay(2)
+
+      // Ensure 2 requests are in-flight and 3 are queued.
+      assertThat(requestLimiter.requestsInCurrentWindow.get()).isEqualTo(2)
+
+      jobs
+    }
+
+    // After all coroutines have finished ensure 3 requests are made
+    val totalRequests = clients.map { it.engine as MockEngine }.sumOf { it.requestHistory.size }
+    assertThat(totalRequests).isEqualTo(5)
+    assertAllJobsAre(jobs) { !isActive }
+    assertAllJobsAre(jobs) { !isCancelled }
+    assertAllJobsAre(jobs) { isCompleted }
+  }
+
+  private fun createClient(
+    requestLimit: Int,
+    windowSize: Duration = 20.milliseconds,
+    responses: MockEngineConfig.() -> Unit,
+  ): HttpClient {
+    return HttpClient(MockEngine(MockEngineConfig().apply { responses(this) })) {
+      install(
+        createClientPlugin("ClientRateLimitPlugin") {
+          requestLimiter = RequestLimiter(requestLimit, windowSize)
+          onRequest { request, _ -> requestLimiter.onNewRequest(request) }
         }
-
-        // After all coroutines have finished ensure 3 requests are made
-        val engine = client.engine as MockEngine
-        assertThat(engine.requestHistory).hasSize(4)
-        assertAllJobsAre(jobs) { !isActive }
-        assertAllJobsAre(jobs) { !isCancelled }
-        assertAllJobsAre(jobs) { isCompleted }
+      )
     }
+  }
 
-    @Test
-    fun `Does not enqueue incoming requests that exceed the request limit for the current window`() {
-        val client = createClient(requestLimit = 2) { repeat(4) { addHandler(instantResponse()) } }
-
-        val jobs = runBlocking {
-            // Launch 4 requests.
-            val jobs = (0..3).map { launch { client.get("/") } }
-
-            delay(2)
-
-            // Ensure 2 requests are in-flight and 2 are queued.
-            assertThat(requestLimiter.requestsInCurrentWindow.get()).isEqualTo(2)
-
-            jobs
-        }
-
-        // After all coroutines have finished ensure 3 requests are made
-        val engine = client.engine as MockEngine
-        assertThat(engine.requestHistory).hasSize(4)
-        assertAllJobsAre(jobs) { !isActive }
-        assertAllJobsAre(jobs) { !isCancelled }
-        assertAllJobsAre(jobs) { isCompleted }
-    }
-
-    @Test
-    fun `delays requests to the new window`() {
-        val client = createClient(requestLimit = 2) { repeat(4) { addHandler(instantResponse()) } }
-
-        runBlocking {
-            val job1 = launch { client.get("/") }
-            val job2 = launch { client.get("/") }
-            delay(2) // ensure jobs are running
-
-            val job3 = launch { client.get("/") }
-            val job4 = launch { client.get("/") }
-            assertThat(requestLimiter.requestsInCurrentWindow.get()).isEqualTo(2)
-
-            delay(10) // ensure jobs 1 and 2 are completed
-
-            assertThat(job1.isCompleted).isTrue()
-            assertThat(job2.isCompleted).isTrue()
-            assertThat(job3.isActive).isTrue()
-            assertThat(job4.isActive).isTrue()
-
-            delay(30) // ensure jobs 3 and 4 are completed
-
-            assertThat(job1.isCompleted).isTrue()
-            assertThat(job2.isCompleted).isTrue()
-            assertThat(job3.isCompleted).isTrue()
-            assertThat(job4.isCompleted).isTrue()
-        }
-
-        val engine = client.engine as MockEngine
-        assertThat(engine.requestHistory).hasSize(4)
-    }
-
-    @Test
-    fun `Enqueues incoming requests that would exceed the requests limit even for different clients`() {
-        val clients =
-            listOf(
-                createClient(requestLimit = 2) { addHandler(instantResponse()) },
-                createClient(requestLimit = 2) { addHandler(instantResponse()) },
-                createClient(requestLimit = 2) { addHandler(instantResponse()) },
-                createClient(requestLimit = 2) { addHandler(instantResponse()) },
-                createClient(requestLimit = 2) { addHandler(instantResponse()) },
-            )
-
-        val jobs = runBlocking {
-            // Launch 5 concurrent requests
-            val jobs = clients.map { client -> launch { client.get("/") } }
-
-            // Minor delay to ensure client.get calls are done
-            delay(2)
-
-            // Ensure 2 requests are in-flight and 3 are queued.
-            assertThat(requestLimiter.requestsInCurrentWindow.get()).isEqualTo(2)
-
-            jobs
-        }
-
-        // After all coroutines have finished ensure 3 requests are made
-        val totalRequests = clients.map { it.engine as MockEngine }.sumOf { it.requestHistory.size }
-        assertThat(totalRequests).isEqualTo(5)
-        assertAllJobsAre(jobs) { !isActive }
-        assertAllJobsAre(jobs) { !isCancelled }
-        assertAllJobsAre(jobs) { isCompleted }
-    }
-
-    private fun createClient(
-        requestLimit: Int,
-        windowSize: Duration = 20.milliseconds,
-        responses: MockEngineConfig.() -> Unit,
-    ): HttpClient {
-        return HttpClient(MockEngine(MockEngineConfig().apply { responses(this) })) {
-            install(
-                createClientPlugin("ClientRateLimitPlugin") {
-                    requestLimiter = RequestLimiter(requestLimit, windowSize)
-                    onRequest { request, _ -> requestLimiter.onNewRequest(request) }
-                }
-            )
-        }
-    }
-
-    private fun assertAllJobsAre(jobs: List<Job>, predicate: Job.() -> Boolean) {
-        assertThat(jobs.all(predicate)).isTrue()
-    }
+  private fun assertAllJobsAre(jobs: List<Job>, predicate: Job.() -> Boolean) {
+    assertThat(jobs.all(predicate)).isTrue()
+  }
 }

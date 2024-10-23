a7f1d25681d157469bc1db73195f12bf68c30723
Bram Wijnands <brambomail@gmail.com>
2024-10-23 13:37:48 +0100

    Reformat using KTFMT.

--- src/main/kotlin/org/audux/bgg/plugin/ClientRateLimitPlugin.kt
+++ src/main/kotlin/org/audux/bgg/plugin/ClientRateLimitPlugin.kt
@@ -16,16 +16,15 @@ package org.audux.bgg.plugin
 import co.touchlab.kermit.Logger
 import io.ktor.client.plugins.api.createClientPlugin
 import io.ktor.client.request.HttpRequestBuilder
-import kotlinx.coroutines.delay
-import org.jetbrains.annotations.VisibleForTesting
 import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.atomic.AtomicReference
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
-import kotlin.time.Duration.Companion.nanoseconds
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.TimeMark
 import kotlin.time.TimeSource
+import kotlinx.coroutines.delay
+import org.jetbrains.annotations.VisibleForTesting
 
 /**
  * Ktor client rate limit plugin.
@@ -34,57 +33,57 @@ import kotlin.time.TimeSource
  * example the plugin can be configured to do 60 requests per minute, or 1 request per second etc.
  */
 internal val ClientRateLimitPlugin =
-  createClientPlugin(
-    "ClientRateLimitPlugin",
-    createConfiguration = ::RequestLimiterConfiguration,
-  ) {
-    val requestLimiter = RequestLimiter(pluginConfig.requestLimit, pluginConfig.windowSize)
-    onRequest { request, _ -> requestLimiter.onNewRequest(request) }
-  }
+    createClientPlugin(
+        "ClientRateLimitPlugin",
+        createConfiguration = ::RequestLimiterConfiguration,
+    ) {
+        val requestLimiter = RequestLimiter(pluginConfig.requestLimit, pluginConfig.windowSize)
+        onRequest { request, _ -> requestLimiter.onNewRequest(request) }
+    }
 
 /**
  * Implementation of [ClientRateLimitPlugin] ensuring not more than [requestLimit] are being within
  * the period of [windowLength].
  */
 internal class RequestLimiter(private val requestLimit: Int, private val windowLength: Duration) {
-  private val timeSource: TimeSource = TimeSource.Monotonic
-  private var currentWindowStart: AtomicReference<TimeMark?> = AtomicReference()
-  private var requestsInDelayed: AtomicInteger = AtomicInteger(0)
+    private val timeSource: TimeSource = TimeSource.Monotonic
+    private var currentWindowStart: AtomicReference<TimeMark?> = AtomicReference()
+    private var requestsInDelayed: AtomicInteger = AtomicInteger(0)
 
-  @VisibleForTesting
-  internal var requestsInCurrentWindow: AtomicInteger = AtomicInteger(0)
+    @VisibleForTesting internal var requestsInCurrentWindow: AtomicInteger = AtomicInteger(0)
 
-  suspend fun onNewRequest(request: HttpRequestBuilder) {
-    do {
-      var windowStart = currentWindowStart.get()
-      if (windowStart == null || windowStart.plus(windowLength).hasPassedNow()) {
-        windowStart = timeSource.markNow()
-        currentWindowStart.set(windowStart)
-        requestsInCurrentWindow.set(0)
-      }
+    suspend fun onNewRequest(request: HttpRequestBuilder) {
+        do {
+            var windowStart = currentWindowStart.get()
+            if (windowStart == null || windowStart.plus(windowLength).hasPassedNow()) {
+                windowStart = timeSource.markNow()
+                currentWindowStart.set(windowStart)
+                requestsInCurrentWindow.set(0)
+            }
 
-      val requestsMadeInWindow = requestsInCurrentWindow.get()
-      if (requestsMadeInWindow < requestLimit) {
-        requestsInCurrentWindow.incrementAndGet()
-        break
-      }
+            val requestsMadeInWindow = requestsInCurrentWindow.get()
+            if (requestsMadeInWindow < requestLimit) {
+                requestsInCurrentWindow.incrementAndGet()
+                break
+            }
 
-      val requestsBeingDelayed = requestsInDelayed.incrementAndGet()
-      logger.i(tag = "RequestLimiter") {
-        "Requests limit for window reached[$requestsMadeInWindow/$requestLimit]"
-      }
+            val requestsBeingDelayed = requestsInDelayed.incrementAndGet()
+            logger.i(tag = "RequestLimiter") {
+                "Requests limit for window reached[$requestsMadeInWindow/$requestLimit]"
+            }
 
-      // Add `requestsBeingDelayed`ms to the delay to ensure requests retain order after delay.
-      delay(
-        windowLength.minus(windowStart.elapsedNow()).plus(requestsBeingDelayed.milliseconds)
-      )
-      requestsBeingDelayed.dec()
-    } while (true)
-  }
+            // Add `requestsBeingDelayed`ms to the delay to ensure requests retain order after
+            // delay.
+            delay(
+                windowLength.minus(windowStart.elapsedNow()).plus(requestsBeingDelayed.milliseconds)
+            )
+            requestsBeingDelayed.dec()
+        } while (true)
+    }
 
-  companion object {
-    private val logger = Logger.withTag("ClientRateLimitPlugin")
-  }
+    companion object {
+        private val logger = Logger.withTag("ClientRateLimitPlugin")
+    }
 }
 
 /**
@@ -96,6 +95,6 @@ internal class RequestLimiter(private val requestLimit: Int, private val windowL
  *   "60 requests per 60.seconds".
  */
 internal data class RequestLimiterConfiguration(
-  var requestLimit: Int = 60,
-  var windowSize: Duration = 60.seconds,
+    var requestLimit: Int = 60,
+    var windowSize: Duration = 60.seconds,
 )
